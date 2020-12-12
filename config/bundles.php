8d40a4f221337c6a735e99ec860c76b6056df119
mamadou.niakate <mamadou.niakate@u-psud.fr>
2020-12-12 02:22:29 +0100

    intro datatables

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     CalendarBundle\CalendarBundle::class => ['all' => true],
+    Omines\DataTablesBundle\DataTablesBundle::class => ['all' => true],
 ];
--- src/Controller/LeadController.php
+++ src/Controller/LeadController.php
@@ -3,9 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Lead;
+use App\Entity\User;
 use App\Form\LeadFormType;
 use App\Repository\LeadRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Omines\DataTablesBundle\Adapter\ArrayAdapter;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
+use Omines\DataTablesBundle\Column\BoolColumn;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\Column\TwigColumn;
+use Omines\DataTablesBundle\DataTableFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,12 +32,36 @@ class LeadController extends AbstractController
 
     /**
      * @Route("/lead", name="lead")
+     * @param Request $request
+     * @param DataTableFactory $dataTableFactory
      * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, DataTableFactory $dataTableFactory): Response
     {
+        $table = $dataTableFactory->create()
+            ->add('first_name', TextColumn::class,['label' => 'Prénom', 'className' => 'bold'])
+            ->add('last_name', TextColumn::class,['label' => 'Nom', 'className' => 'bold'])
+            ->add('website', TextColumn::class,['label' => 'Site Web', 'className' => 'bold'])
+            ->add('phone', TextColumn::class,['label' => 'Téléphone', 'className' => 'bold'])
+            ->add('email', TextColumn::class,['label' => 'Email', 'className' => 'bold'])
+            ->add('industry', TextColumn::class,['label' => 'Industrie', 'className' => 'bold'])
+            ->add('street', TextColumn::class,['label' => 'Rue', 'className' => 'bold'])
+            ->add('zipcode', TextColumn::class,['label' => 'Code Postale', 'className' => 'bold'])
+            ->add('city', TextColumn::class,['label' => 'Ville', 'className' => 'bold'])
+            ->add('assigned_to', TextColumn::class, ['field' => 'assigned_to.first_name','label' => 'Assigné À', 'className' => 'bold'])
+            ->add('status', TextColumn::class,  ['field' => 'status.status', 'label' => 'Status', 'className' => 'bold'])
+            ->createAdapter(ORMAdapter::class, [
+                'entity' => Lead::class
+            ])
+            ->handleRequest($request);
+
+        if($table->isCallback()) {
+            return $table->getResponse();
+        }
+
         return $this->render('lead/index.html.twig', [
-            'leads' => $this->leadRepository->findAll()
+            'leads' => $this->leadRepository->findAll(),
+            'datatable' => $table
         ]);
     }
 
